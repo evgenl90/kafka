7f9138cf5b9d616c04b4facb924a50ce054c9d0b
Evgen <evgen.990029@gmail.com>
2022-08-20 12:44:43 +0300

    Initial commit

---
+++ src/main/java/com/example/kafka/MsgController.java
@@ -0,0 +1,23 @@
+package com.example.kafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("msg")
+public class MsgController {
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @PostMapping
+    public void sendMsg(String msgId, String msg){
+        ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send("msg", msgId, msg);
+        future.addCallback(System.out::println, System.err::println);
+        kafkaTemplate.flush();
+    }
+}
